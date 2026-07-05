c338aacc5aba74f956fa93b77eba2b965b2327e0
Lebogang Mokoena <ditswe24@gmail.com>
2026-07-05 13:31:38 +0200

    Implement SortSelect component for sorting optionsrefactor(DJS05): update context-consuming components to use split context  - SearchBar, SortSelect, GenreFilter, Pagination, PodcastGrid, Home now   import PodcastContext from context/podcastContext.js - SortSelect imports SORT_OPTIONS from data.js
    Added a dropdown component for selecting sort order using context.

--- src/components/SortSelect.jsx
+++ src/components/SortSelect.jsx
@@ -1,1 +1,25 @@
+import { useContext } from "react";
+import { PodcastContext } from "../context/podcastContext";
+import { SORT_OPTIONS } from "../data";
+import styles from "./SortSelect.module.css";
 
+/**
+ * Dropdown for choosing sort order.
+ */
+export default function SortSelect() {
+  const { sortKey, setSortKey } = useContext(PodcastContext);
+
+  return (
+    <select
+      className={styles.select}
+      value={sortKey}
+      onChange={(e) => setSortKey(e.target.value)}
+    >
+      {SORT_OPTIONS.map((o) => (
+        <option key={o.key} value={o.key}>
+          {o.label}
+        </option>
+      ))}
+    </select>
+  );
+}
